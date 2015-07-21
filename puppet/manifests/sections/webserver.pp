723eb6e05c64e70172f821dff763e4f63a90bf44
Micah Ernst <micahwave@gmail.com>
2015-07-21 12:15:48 -0400

    Updating config files for qz.dev

--- puppet/manifests/sections/webserver.pp
+++ puppet/manifests/sections/webserver.pp
@@ -5,7 +5,7 @@ class { 'nginx':
   types_hash_max_size  => '2048',
 }
 
-nginx::vhost { '_':
+nginx::vhost { 'qz.dev':
   docroot    => '/srv/www',
   template   => 'nginx/vip.dev.erb',
   owner      => 'www-data',
